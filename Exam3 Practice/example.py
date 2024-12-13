d4a035db43ff913f02134d36fa96b3154c961b58
Saransh Rakshak <srakshak@berkeley.edu>
2024-12-13 15:39:55 -0800

    Added missing lecture files

---
+++ Exam3 Practice/example.py
@@ -0,0 +1,13 @@
+'an example module'
+import sys
+
+def f():
+    'function f'
+    print('Executing f() from', __name__)
+
+def g():
+    'function g'
+    print('Executing g() from', __name__)
+    
+x = 100  # global var
+#print('example here')
